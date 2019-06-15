d5c6fc1a72b2f75b02d674e95d31735177e649c3
Nicholas Rempel <nbrempel@gmail.com>
2019-06-15 11:04:24 -0700

    update
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Package Boilerplate
+
+This is an minimal example of how to publish a python package.
--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ import runpy
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "package-boilerplate"
-version_meta = runpy.run_path("./version.py".format(PACKAGE_NAME))
+version_meta = runpy.run_path("./version.py")
 VERSION = version_meta["__version__"]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 
 def parse_requirements(filename):
@@ -19,8 +23,10 @@ if __name__ == "__main__":
         name=PACKAGE_NAME,
         version=VERSION,
         packages=find_packages(),
-        # include_package_data=True,
         install_requires=parse_requirements("requirements.txt"),
         python_requires=">=3.6.3",
         scripts=["scripts/boilerplate-cli"],
+        description="This is a description.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
     )
